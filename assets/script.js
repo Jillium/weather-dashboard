7ddf4d6caceab8e4ff81c37e1df406f68b0d5ae5
Jillium <86424914+Jillium@users.noreply.github.com>
2021-10-17 12:28:58 -0600

    add button event listeners

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,21 @@
-console.log("hello");
+
+
+
+
+document.getElementById("search-btn").addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("click click");
+})
+
+
+
+
+
+
+// event listener for all of the city buttons after they are created 
+btns = document.getElementsByClassName("city-btn");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        console.log("i was clicked");
+    })
+}
