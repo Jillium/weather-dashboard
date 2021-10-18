45e0f51392cd0733443204e2aebf3a8bd7b494ff
Jillium <86424914+Jillium@users.noreply.github.com>
2021-10-18 11:38:27 -0600

    added second api to call current data from latitude and longitude

--- assets/script.js
+++ assets/script.js
@@ -3,8 +3,9 @@ var submitButton = document.querySelector("#search-btn");
 
 
 
-// user types city into search
-// search button is clicked which does the api call. api call returns data on city that is searched
+
+
+
 // data from api call is dynamically added to page. Current selected city appears and icon appears with weather icon
 //temperature, wind, humidity and uv index are dynamically added. UV index background changes color based off of index level
 // 5 day forecast for selected city is dynamically added
@@ -17,27 +18,46 @@ var submitButtonHandler = function(event) {
     event.preventDefault();
     // get city value from input element
     var currentCity = cityInputEl.value.trim();
-
+    // if they entered a city 
     if (currentCity) {
-        getWeather(currentCity);
+        // this is running the getCity function 
+        getCity(currentCity);
         cityInputEl.value = "";
         
     }
+    // if they didn't enter a valid input
     else {
         alert("please enter a city");
     }
+    
 }
 
 
-// function to get selected city info from api call 
-var getWeather = function(currentCity) {
+// function to get selected city latitude and longitude info from api call 
+var getCity = function(currentCity) {
     // this variable allows user to select a city and creates the url to search for it 
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + currentCity + "&appid=096aee0d213daa88cdc744bebc199f14";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + currentCity + "&units=imperial&appid=096aee0d213daa88cdc744bebc199f14";
     // fetch request to get info from url we just created 
     fetch(apiUrl).then(function(response) {
     // takes response and changes it into data we can use 
     response.json().then(function(data) {
-        console.log(data);
+        console.log(data.coord.lat);
+        console.log(data.coord.lon);
+        var lat = data.coord.lat;
+        var lon = data.coord.lon;
+
+        var apiUrl2 = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=096aee0d213daa88cdc744bebc199f14";
+
+        fetch(apiUrl2).then(function(response) {
+            response.json().then(function(data) {
+                console.log(data);
+                console.log(data.current.temp);
+                console.log(data.current.wind_speed);
+                console.log(data.current.humidity);
+                console.log(data.current.uvi);
+            })
+        })
+        
         
     });
     });
@@ -45,6 +65,9 @@ var getWeather = function(currentCity) {
     
 
 };
+
+
+
 
 
 
